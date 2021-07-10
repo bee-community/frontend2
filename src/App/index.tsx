ef697b4bed43051825bf2935e8437b9b7c4ed3db
Jeonghye Choi <ppllhm.0@gmail.com>
2021-07-11 02:25:30 +0900

    feat(pages): add router for pages
    feat #5

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import MyPage from '../pages/MyPage';
+import PostAdd from '../pages/PostAdd';
+import PostDetail from '../pages/PostDetail';
 
-function App() {
-  return <div className="App">Hello world</div>;
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/">
+      <Redirect to="/home" />
+    </Route>
+    <Route path="/home" component={Home} />
+    <Route path="/mypage" component={MyPage} />
+    <Route path="/postDetail" component={PostDetail} />
+    <Route path="/postAdd" component={PostAdd} />
+  </Switch>
+);
 
 export default App;
