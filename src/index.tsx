291df10b29b0eb4cd858a224064708757d097883
Jeonghye Choi <ppllhm.0@gmail.com>
2021-07-31 06:35:03 +0900

    feat(globalstyle): add global style
    #7

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { ThemeProvider } from "@emotion/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "styles/global";
+import theme from "styles/theme";
+
+import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
