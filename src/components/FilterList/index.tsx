37beee1ce311e9d4d2d9db1c6464af9df30ef098
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-15 00:16:23 +0900

    feat(filter): add filter list styles
    #7

--- src/components/FilterList/index.tsx
+++ src/components/FilterList/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import Filter from "components/Filter";
-import { FilterZone } from "./styles";
+import React from 'react';
+import Filter from 'components/Filter';
+import { FilterZone, FilterWrap, Swiper, SwiperButton } from './styles';
 
 const FilterList = () => {
   return (
     <FilterZone>
-      <Filter key="" />
+      <FilterWrap>
+        <Filter title="" key="" />
+        <Filter title="" key="" />
+        <Filter title="" key="" />
+      </FilterWrap>
+
+      <Swiper>
+        <SwiperButton>왼</SwiperButton>
+        <SwiperButton>오</SwiperButton>
+      </Swiper>
     </FilterZone>
   );
 };
--- src/components/FilterList/styles.tsx
+++ src/components/FilterList/styles.tsx
@@ -1,3 +1,42 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-export const FilterZone = styled.div``;
+export const FilterZone = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  height: 10vh;
+  margin-bottom: 30px;å
+
+  ${(props) => props.theme.mq.mobile}{
+    height: 7vh;
+    margin-bottom: 10px;
+  }
+`;
+
+export const FilterWrap = styled.div`
+  display: flex;
+  flex-grow: 1;
+  height: 100%;
+  background: #ffffc3;
+`;
+
+export const Swiper = styled.div`
+  width: 80px;
+  height: 10vh;
+  background: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  ${(props) => props.theme.mq.mobile}{
+    height: 7vh;
+  }
+`;
+
+export const SwiperButton = styled.button`
+  width: 30px;
+  height: 30px;
+  border: none;
+  border-radius: 50%;
+`;
