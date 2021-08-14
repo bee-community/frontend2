eaf9c276e0c03a3cd9e0e51836aa6d8156988e25
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-14 23:52:34 +0900

    feat: add loadable component for code splitting
    #7

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
-import Home from "../pages/Home";
-import MyPage from "../pages/MyPage";
-import PostAdd from "../pages/PostAdd";
-import PostDetail from "../pages/PostDetail";
+import React from 'react';
+import loadable from '@loadable/component';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+const Home = loadable(() => import('pages/Home'));
+const MyPage = loadable(() => import('pages/MyPage'));
+const PostAdd = loadable(() => import('pages/PostAdd'));
+const PostDetail = loadable(() => import('pages/PostDetail'));
 
 const App = () => (
   <Switch>
-    <Route exact path="/">
-      <Redirect to="/home" />
-    </Route>
+    <Redirect exact path="/" to="/home" />
     <Route path="/home" component={Home} />
     <Route path="/mypage" component={MyPage} />
     <Route path="/postDetail" component={PostDetail} />
