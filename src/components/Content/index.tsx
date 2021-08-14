c03e5006e00acf2b2d2c9b9773da67c6926be003
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-15 00:04:00 +0900

    feat(content): add content and content list
    #7

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React from 'react';
+import testImg from 'assets/images/test_img.jpg';
+import Icon from '@mdi/react';
+import {
+  mdiBookmarkOutline,
+  mdiBookmark,
+  mdiHeartOutline,
+  mdiHeart,
+  mdiMessageOutline,
+  mdiAccountOutline,
+} from '@mdi/js';
+import {
+  ContentWraper,
+  PostMainImage,
+  PostImg,
+  DescribeWraper,
+  Describe,
+  Title,
+  AboutWrap,
+  Likes,
+  Comments,
+  NickName,
+} from './styles';
 
-const Content = () => {
-  return <>Content 입니다</>;
-};
+const Content = () => (
+  <ContentWraper>
+    <PostMainImage>
+      <PostImg src={testImg} alt="test img" />
+    </PostMainImage>
+
+    <DescribeWraper>
+      <Describe>
+        <Title>글의 타이틀이죠</Title>
+        <AboutWrap>
+          <Icon path={mdiHeartOutline} title="likes" size={1} />
+          <Likes>500</Likes>
+          <Icon path={mdiMessageOutline} title="comment" size={1} />
+          <Comments>22</Comments>
+          <Icon path={mdiAccountOutline} title="nickname" size={1} />
+          <NickName>닉네임</NickName>
+        </AboutWrap>
+      </Describe>
+      <Icon path={mdiBookmarkOutline} title="book mark" size={1} />
+    </DescribeWraper>
+  </ContentWraper>
+);
 
 export default Content;
--- src/components/ContentList/styles.tsx
+++ src/components/ContentList/styles.tsx
@@ -1,3 +1,20 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import theme from 'styles/theme';
 
-export const ContentZone = styled.div``;
+export const ContentZone = styled.div`
+  flex-grow: 1;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 28px 28px;
+  align-items: stretch;
+  align-content: start;
+  overflow: scroll;
+
+  >* {
+    /* background: #dddddd; */
+  }
+
+  ${(props) => props.theme.mq.mobile}{
+    gap: 10px 10px;
+  }
+`;
