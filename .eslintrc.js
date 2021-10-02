d2f393ecdcd77ad0e7342dfcfb1f4cdff520e966
jeonghye-choi <ppllhm.0@gmail.com>
2021-10-02 15:24:16 +0900

    fix(eslint): fix parsing error: "parserOptions.project" has been set for @typescript-eslint/parser
    #16

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    // tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true,
     },
