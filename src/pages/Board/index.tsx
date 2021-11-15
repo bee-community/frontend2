38f840c251424af7b165f02444bd6796c0486b48
jeonghye-choi <ppllhm.0@gmail.com>
2021-11-15 13:26:10 +0900

    feat(board): add articles get reqeust
    #26

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import Http from 'api';
+import { useCallback, useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+
+import { ArticleListContainer, Article } from './styles';
 
 const Board = () => {
-  return <>Board</>;
+  let { boardId } = useParams();
+  const [articles, setArticles] = useState([]);
+
+  const getArticles = useCallback(() => {
+    Http.get(`/boards/${boardId}`)
+      .then(response => {
+        setArticles(response.data['articles']);
+      })
+      .catch(error => console.log(error))
+      .finally(() => {});
+  }, [boardId]);
+
+  useEffect(() => {
+    getArticles();
+  }, [getArticles]);
+
+  return (
+    <>
+      <div>어떤어떤 게시판</div>
+      <ArticleListContainer>
+      </ArticleListContainer>
+    </>
+  );
 };
 
 export default Board;
