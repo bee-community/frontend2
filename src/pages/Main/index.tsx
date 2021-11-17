ce9eeac29fa778a7db7b946dc2d8519b643f5524
jeonghye-choi <ppllhm.0@gmail.com>
2021-11-17 12:36:15 +0900

    feat(main): add main structure and styles
    #28

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {
+  MainWrap,
   Section,
   Title,
   Category,
@@ -14,7 +15,7 @@ import {
 
 const Main = () => {
   return (
-    <>
+    <MainWrap>
       <Section>
         <Title />
         <Category />
@@ -26,7 +27,7 @@ const Main = () => {
         <DailyList />
         <WeeklyList />
       </Aside>
-    </>
+    </MainWrap>
   );
 };
 
