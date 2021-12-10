07d631c9cfcc0c57b41ecf1abed9772c628fd2c0
jeonghye-choi <ppllhm.0@gmail.com>
2021-12-11 08:44:44 +0900

    style(html): fix html default font size to 16px
    #28

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,16 +2,16 @@ import { css, SerializedStyles, Theme } from '@emotion/react';
 
 const globalStyle = (theme: Theme): SerializedStyles => css`
   html {
-    font-size: 10px;
+    font-size: 16px;
   }
   body {
     margin: 0;
     padding: 0;
-    font-size: 1.6rem;
     font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo',
       Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic',
       sans-serif;
     background-color: ${theme.palette.background.yellow};
+    color: ${theme.palette.text.primary};
   }
 `;
 
