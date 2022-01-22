b4d31ccd02f391eff27680e0bed9f9d101e3d07f
jeonghye-choi <ppllhm.0@gmail.com>
2022-01-22 15:23:48 +0900

    feat(router): add mypage subpages router
    #33

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import Layout from 'layouts/Layout';
 import ArticleDetail from 'pages/ArticleDetail';
 import ArticlePost from 'pages/ArticlePost';
 import Board from 'pages/Board';
+import EditPrivacy from 'pages/EditPrivacy';
+import FAQ from 'pages/FAQ';
 import LogIn from 'pages/LogIn';
 import Main from 'pages/Main';
 import Mypage from 'pages/Mypage';
+import Question from 'pages/Question';
+import Report from 'pages/Report';
+import RequestBoard from 'pages/RequestBoard';
 import SignUp from 'pages/SignUp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -17,8 +22,15 @@ function App() {
           <Route path="board/:boardId" element={<Board />} />
           <Route path="article/post" element={<ArticlePost />} />
           <Route path="article/:articleId" element={<ArticleDetail />} />
+
           <Route path="mypage" element={<Mypage />} />
+          <Route path="mypage/edit-privacy" element={<EditPrivacy />} />
+          <Route path="mypage/FAQ" element={<FAQ />} />
+          <Route path="mypage/question" element={<Question />} />
+          <Route path="mypage/report" element={<Report />} />
+          <Route path="mypage/request-board" element={<RequestBoard />} />
         </Route>
+
         <Route path="signup" element={<SignUp />} />
         <Route path="login" element={<LogIn />} />
       </Routes>
