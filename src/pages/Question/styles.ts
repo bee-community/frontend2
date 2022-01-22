d1fc30eb266f132b3c55819e02e491d59b1ca527
jeonghye-choi <ppllhm.0@gmail.com>
2022-01-22 22:16:09 +0900

    feat: add Table component
    #32

--- src/pages/Question/styles.ts
+++ src/pages/Question/styles.ts
@@ -21,60 +21,6 @@ export const Form = styled.form`
   align-items: center;
 `;
 
-export const QuestionTable = styled.table`
-  width: 100%;
-  border-top: solid 1px #aaa;
-  border-bottom: solid 1px #aaa;
-  text-align: left;
-  border-spacing: unset;
-  margin-bottom: 30px;
-  font-size: 1rem;
-
-  .title {
-    border-bottom: solid 1px #ddd;
-  }
-
-  th {
-    width: 20%;
-    min-width: 60px;
-    padding: 20px;
-    background: #f4f4f4;
-    vertical-align: top;
-  }
-
-  td {
-    padding: 15px;
-
-    input {
-      font-size: 1rem;
-      width: 100%;
-      padding: 7px 5px;
-      border-radius: 5px;
-      border: solid 1px #ddd;
-    }
-    input:focus,
-    textarea:focus {
-      outline: none;
-    }
-    input::placeholder,
-    textarea::placeholder {
-      font-weight: 300;
-      color: #777;
-    }
-
-    textarea {
-      font-family: NotoSansCJKKR;
-      font-size: 1rem;
-      width: 100%;
-      min-height: 30vh;
-      padding: 7px 5px;
-      border-radius: 5px;
-      border: solid 1px #ddd;
-      resize: none;
-    }
-  }
-`;
-
 export const Button = styled.button`
   padding: 15px 68px;
   border-radius: 32px;
