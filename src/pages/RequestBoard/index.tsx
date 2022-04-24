506f820b84c6ff3c4c2c8745de29655d2d5c12e0
jeonghye-choi <ppllhm.0@gmail.com>
2022-04-24 22:56:55 +0900

    feat: apply `Button` atom component to `RequestBoard`
    #47

--- src/pages/RequestBoard/index.tsx
+++ src/pages/RequestBoard/index.tsx
@@ -1,5 +1,6 @@
 import RequestBox from 'components/RequestBox';
 import { ShadowBox } from 'components/ShadowBox';
+import Button from 'components/atoms/Button';
 import * as React from 'react';
 import { useState } from 'react';
 
@@ -12,7 +13,15 @@ function RequestBoard() {
     <>
       <Title>
         <span>게시판 추가요청</span>
-        <button className="new-board-request">새로운 게시판 요청</button>
+        <Button
+          buttonType="contained"
+          color="yellow"
+          radius="round"
+          css={{
+            padding: '12px 28px 11px',
+          }}>
+          새로운 게시판 요청
+        </Button>
       </Title>
       <ShadowBox>
         <RequestBoxsWrap>
