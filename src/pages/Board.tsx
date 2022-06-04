d4c0db6dfca2deb235646121a90914b189199cbb
jeonghye-choi <ppllhm.0@gmail.com>
2022-06-04 18:29:09 +0900

    feat(board): add test article datas
    아직 로그인 구현을 안했기 때문에 권한이 필요한 article create를 못해서 테스트 데이터로 작업
    
    #49

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -2,7 +2,7 @@ import BoardTitleContainer from 'components/molecules/containers/BoardTitleConta
 import BoardArticleList from 'components/organisms/lists/BoardArticleList';
 import { useArticlesState } from 'context/Article';
 import useArticleActions from 'hooks/useArticleActions';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 
 function Board() {
@@ -12,12 +12,118 @@ function Board() {
 
   useEffect(() => {
     articleActions.getArticles(boardName);
-  }, []);
+
+  const [dummyArticles] = useState([
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '1234354657687989012343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+    {
+      title: '지금 메가마트에서 메가 세일해',
+      tags: '#꿀팁 #꿀팁 #꿀팁',
+      board_id: '12343546576879890',
+      likes: 11,
+      comments: 23,
+      created_at: '2021.10.01 4:00',
+    },
+  ]);
 
   return (
     <>
       <BoardTitleContainer title={articlesData.name} />
-      <BoardArticleList articles={articlesData.articles} />
+      <BoardArticleList articles={dummyArticles} />
     </>
   );
 }
