ea83bfe1dd1fb041e7c04ae845f29d9a929b7f92
jeonghye-choi <ppllhm.0@gmail.com>
2022-06-18 18:46:42 +0900

    fix(article): change dummyArticle data matching changed format
    #56

--- src/components/organisms/lists/BestArticleList/BestArticleList.tsx
+++ src/components/organisms/lists/BestArticleList/BestArticleList.tsx
@@ -2,19 +2,10 @@ import ArticleBox from 'components/molecules/boxes/ArticleBox';
 import Title, {
   TitleContainerProps,
 } from 'components/molecules/containers/TitleContainer';
+import { ArticleType } from 'context/Articles';
 import * as React from 'react';
 
 import { StyledBestArticleList } from './styles';
-
-export interface ArticleType {
-  id: string;
-  title: string;
-  tags: string;
-  board_id: string;
-  likes: number;
-  comments: number;
-  created_at: string;
-}
 
 export interface BestArticleListProps extends TitleContainerProps {
   articles: ArticleType[];
--- src/components/organisms/lists/BoardArticleList/BoardArticleList.tsx
+++ src/components/organisms/lists/BoardArticleList/BoardArticleList.tsx
@@ -1,5 +1,6 @@
 import arrowDown from 'assets/images/icons/arrow-down.png';
 import ArticleBox from 'components/molecules/boxes/ArticleBox';
+import { ArticleType } from 'context/Articles';
 import * as React from 'react';
 
 import { StyledBoardArticleList } from './styles';
@@ -12,7 +13,7 @@ function BoardArticleList(props: any) {
         <img src={arrowDown} alt="﹀" />
       </div>
       <div className="board-article-list-wrap">
-        {props.articles.map((article: any, index: number) => {
+        {props.articles.map((article: ArticleType, index: number) => {
           return <ArticleBox key={index} article={article} />;
         })}
       </div>
