488dcc6c4adf023586022dae1ed16285c2cc02ee
jeonghye-choi <ppllhm.0@gmail.com>
2022-06-18 18:32:51 +0900

    feat(article): add `TagRecommendList` organism component
    #56

---
+++ src/components/organisms/lists/TagRecommendList/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const StyledTagRecommendList = styled.section`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 26px;
+
+  & > div {
+    margin-right: 18px;
+    margin-bottom: 10px;
+  }
+`;
