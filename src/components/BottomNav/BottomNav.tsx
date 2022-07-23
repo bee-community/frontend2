00ba3ddc459eebc840f2f214403aa6d464d93236
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-07-23 17:37:59 +0900

    바텀 네비게이션바 구현

---
+++ src/components/BottomNav/BottomNav.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+import activechat from '../../assets/chatImages/activechat.png';
+import activehome from '../../assets/chatImages/activehome.png';
+import activemenu from '../../assets/chatImages/activemenu.png';
+import activemypage from '../../assets/chatImages/activemypage.png';
+import chat from '../../assets/chatImages/chat.png';
+import home from '../../assets/chatImages/home.png';
+import menu from '../../assets/chatImages/menu.png';
+import mypage from '../../assets/chatImages/mypage.png';
+import './BottomNav.css';
+
+const BottomNav = () => {
+  const [page, setPage] = useState('home');
+  return (
+    <>
+      <nav className="bottomNavWrapper">
+        <div>
+          <Link to="/">
+            {page === 'menu' ? (
+              <img className="img" src={activemenu} alt=""></img>
+            ) : (
+              <img
+                role="presentation"
+                onClick={() => {
+                  setPage('menu');
+                }}
+                className="img"
+                src={menu}
+                alt=""></img>
+            )}
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            {page === 'home' ? (
+              <img src={activehome} alt=""></img>
+            ) : (
+              <img
+                role="presentation"
+                onClick={() => {
+                  setPage('home');
+                }}
+                src={home}
+                alt=""></img>
+            )}
+          </Link>
+        </div>
+        <div>
+          <Link to="/chat">
+            {page === 'chat' ? (
+              <img src={activechat} alt=""></img>
+            ) : (
+              <img
+                role="presentation"
+                onClick={() => {
+                  setPage('chat');
+                }}
+                src={chat}
+                alt=""></img>
+            )}
+          </Link>
+        </div>
+        <div>
+          <Link to="/mypage">
+            {page === 'mypage' ? (
+              <img src={activemypage} alt=""></img>
+            ) : (
+              <img
+                role="presentation"
+                onClick={() => {
+                  setPage('mypage');
+                }}
+                src={mypage}
+                alt=""></img>
+            )}
+          </Link>
+        </div>
+      </nav>
+      <div className="bottomNavTrick"></div>
+    </>
+  );
+};
+
+export default BottomNav;
