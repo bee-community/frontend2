7745f3cbd981e735b91139ac418e469a852ba961
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-07 19:30:30 +0900

    fix: 스크롤바 hidden에서 기능만 살리고 안보이도록 변경

--- src/components/ChatChat/styles.ts
+++ src/components/ChatChat/styles.ts
@@ -1,5 +1,18 @@
 import styled from '@emotion/styled';
 
+export const ModalBackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  transition: 0.5s;
+  width: 360px;
+  height: 507px;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.8);
+`;
+
 export const ChatBox = styled.div`
   border-radius: 15px;
   height: 507px;
@@ -7,11 +20,16 @@ export const ChatBox = styled.div`
   background-color: white;
   overflow-x: hidden;
   position: relative;
-  overflow-y: hidden;
+  /* overflow-y: hidden; */
   @media (max-width: ${props => props.theme.screenSize.md}) {
     width: calc(var(--vw) * 100);
     height: calc(var(--vh) * 100);
     border-radius: 0px;
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+    width: 0 !important;
   }
 `;
 
