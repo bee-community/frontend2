a40c6217ab868a1577356654d023f22ebb629b8e
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-05 17:15:44 +0900

    feat: 메인 페이지로 이동시 user 정보를 요청
    왜? : 로그인을 하고 메인페이지로 이동할때 유저정보를 얻어야 상단 닉네임텍스트가 변경이 가능하였다.

--- src/components/Templates/Template.tsx
+++ src/components/Templates/Template.tsx
@@ -1,10 +1,28 @@
+import axios from 'axios';
 import Aside from 'components/Aside';
 import Header from 'components/organisms/Header';
+import { useAuthState } from 'context/Auth';
+import API from 'mainAPI';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router';
 
 import { StyledTemplate, StyledMain, Main, StyledAside } from './styles';
 
 function Template() {
+  const auth = useAuthState();
+  API.get('/users/self', {
+    headers: {
+      Authorization: `${auth.tokenType} ${auth.accessToken}`,
+    },
+  })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {});
+
   return (
     <StyledTemplate>
       <Header />
