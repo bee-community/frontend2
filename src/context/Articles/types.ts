9d3d1e0f0f5702d7521eb3c830b433590e6c5a64
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-12-01 09:34:51 +0900

    feat: SpecificBoardArticlesType 구현

--- src/context/Articles/types.ts
+++ src/context/Articles/types.ts
@@ -32,27 +32,10 @@ export type ArticleType = {
   updated_at: string;
 };
 
-// {ㅋ
-//   "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//   "title": "string",
-//   "content": "string",
-//   "summary": "string",
-//   "board_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//   "view_count": 0,
-//   "is_announcement": true,
-//   "like_count": 0,
-//   "tag_links": [],
-//   "poll": {
-//     "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//     "title": "string",
-//     "is_multiple": false,
-//     "contents": [
-//       {
-//         "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//         "content": "string"
-//       }
-//     ]
-//   },
-//   "created_at": "2022-11-11T15:24:14.019Z",
-//   "updated_at": "2022-11-11T15:24:14.019Z"
-// }
+export type SpecificBoardArticlesType = {
+  id: string;
+  name: string;
+  path: string;
+  articles: ArticleType[];
+  announcements: any[];
+};
