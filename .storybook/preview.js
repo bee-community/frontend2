806c86ee22cca487a0c2bb053a9489887b7e6280
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-12-26 13:16:11 +0900

    feat: #12 - 스토리북에서 theme 을 사용할 수 있도록 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import { store } from '../src/store';
 import { Provider } from 'react-redux';
-
+import {  ThemeProvider } from '@emotion/react';
+import theme from 'assets/theme';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -13,8 +14,10 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
+    <ThemeProvider theme={theme}>
     <Provider store={store}>
       <Story />
     </Provider>
+    </ThemeProvider>
   ),
 ]
