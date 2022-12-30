ae162c7f6b7461b695f7c9d52ff0ea7732ab6a3c
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-12-30 15:07:41 +0900

    feat: 배너 클릭시 회원가입으로 이동

--- src/components/molecules/boxes/BannerBox/BannerBox.tsx
+++ src/components/molecules/boxes/BannerBox/BannerBox.tsx
@@ -1,14 +1,16 @@
 import Button from 'components/atoms/Button';
 import * as React from 'react';
+import { useNavigate } from 'react-router';
 
 import { StyledBannerBox } from './styles';
 
 function BannerBox() {
+  const navigate = useNavigate();
   return (
     <StyledBannerBox>
       <span>다양한 사람들 속에서 닮은 꼴 찾기</span>
       <span>꿀벌커뮤니티</span>
-      <Button buttonType="contained" color="black" radius="round">
+      <Button onClick={() => navigate('/signup')} buttonType="contained" color="black" radius="round">
         회원가입하고 포인트 받기
       </Button>
     </StyledBannerBox>
