b65a7780979f20fe11db62b962d886841cfb341d
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-01-02 10:14:54 +0900

    feat: 페이지 이동시 스크롤 위치를 최상단으로 이동시킨다.

--- src/components/Templates/Template.tsx
+++ src/components/Templates/Template.tsx
@@ -2,21 +2,29 @@ import Aside from 'components/Aside';
 import Header from 'components/organisms/Header';
 import { useGetUserSelf } from 'hooks/queries/requests';
 import useUserInfo from 'hooks/useUserInfo';
+import { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router';
+import { useLocation } from 'react-router-dom';
 
 import { StyledTemplate, StyledMain, Main, StyledAside } from './styles';
 
 function Template() {
+  const mainRef = useRef<HTMLElement>(null);
   useUserInfo();
   useGetUserSelf();
   const { articleOpen } = useSelector((store: any) => store.openState);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    mainRef.current?.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <StyledTemplate articleOpen={articleOpen}>
       <Header />
       <StyledMain>
-        <Main>
+        <Main ref={mainRef}>
           <Outlet />
         </Main>
         <StyledAside>
