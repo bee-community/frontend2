b3313f491a499159ef3b4d5244c76644d9bcf3d2
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-01-05 21:39:06 +0900

    feat: 스토리북 addons에 storybook-preset-craco 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,12 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-preset-craco',
+  ],
   framework: '@storybook/react',
   webpackFinal: async config => {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
