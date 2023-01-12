d44a6d58278af7ca5b297f7a76d7f226e97bf55b
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-01-12 22:46:33 +0900

    feat: 대댓글 생성 hook 구현

---
+++ src/@hooks/ui/comment/useReplyForm.ts
@@ -0,0 +1,37 @@
+import { FormEventHandler, useState, useCallback } from 'react';
+
+import { useCreateComment } from '@hooks/business/article';
+
+interface useReplyFromProps {
+  commentId: string;
+  articleId: string;
+}
+export const useReplyForm = (props: useReplyFromProps) => {
+  const { commentId, articleId } = props;
+  const [reply, setReply] = useState('');
+  const onChangeReply = useCallback(e => {
+    setReply(e.target.value);
+  }, []);
+
+  const { createComment } = useCreateComment();
+
+  const onSubmitReplyCreateForm: FormEventHandler<HTMLFormElement> = async e => {
+    e.preventDefault();
+    createComment({
+      body: {
+        content: reply,
+        parent_id: commentId,
+      },
+      articleId: articleId,
+    });
+    setReply('');
+  };
+
+  return {
+    state: { reply },
+    handler: {
+      onChangeReply,
+      onSubmitReplyCreateForm,
+    },
+  };
+};
