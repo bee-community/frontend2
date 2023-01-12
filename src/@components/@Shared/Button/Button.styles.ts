baea27673225069317587ed455f7ac8844c2aba3
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-01-12 21:45:15 +0900

    refactor: 버튼의 공통 css 변경

--- src/@components/@Shared/Button/Button.styles.ts
+++ src/@components/@Shared/Button/Button.styles.ts
@@ -1,17 +1,43 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 import styled from '@emotion/styled';
+import theme from 'styles/theme';
 
-const Common = css`
-  padding: 10px;
-
+const Common = (theme: Theme) => css`
+  padding: 7px 20px;
+  border-radius: 44px;
+  background: none;
   border: none;
-  border-radius: 10px;
-
+  font-family: NotoSansCJKKR;
+  font-size: 1rem;
+  color: #111;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   cursor: pointer;
+
+  @media (max-width: ${theme.screenSize.xl}) {
+    padding: 7px 16px;
+    font-size: ${theme.fontSize[14]};
+  }
+
+  @media (max-width: ${theme.screenSize.mobileL}) {
+    padding: 5px 10px;
+    font-size: ${theme.fontSize[11]};
+  }
+
+  @media (max-width: ${theme.screenSize.mobileM}) {
+    padding: 7px 12px;
+    font-size: ${theme.fontSize[10]};
+  }
+
+  @media (max-width: ${theme.screenSize.mobileS}) {
+    padding: 7px 12px;
+    font-size: ${theme.fontSize[8]};
+  }
 `;
 
 export const DefaultButton = styled.button`
-  ${Common}
+  ${Common(theme)}
 
   ${({ theme }) => css`
     background-color: ${theme.palette.yellow[100]};
@@ -20,7 +46,7 @@ export const DefaultButton = styled.button`
 `;
 
 export const OutlineButton = styled.button`
-  ${Common}
+  ${Common(theme)}
 
   ${({ theme }) => css`
     border: solid 1px #707070;
