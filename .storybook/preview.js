528717eef12fe624dc9c6784f11dd87808c811d0
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-01-13 20:29:58 +0900

    fix: QueryClientProvider가 없어서 생기는 오류 해결

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,10 @@ import { store } from '../src/store';
 import { Provider } from 'react-redux';
 import {  ThemeProvider } from '@emotion/react';
 import theme from 'styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -16,7 +20,9 @@ export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
     <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
       <Story />
+      </QueryClientProvider>
     </Provider>
     </ThemeProvider>
   ),
