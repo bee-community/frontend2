dfc5efe0e69ce96e427063ed66bf39d42ef39d59
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-12 00:15:30 +0900

    refactor: url 변경

--- src/apis/article.ts
+++ src/apis/article.ts
@@ -1,13 +1,7 @@
 import { client } from 'apis';
-import axios from 'axios';
 import { CreateArticleRequest, CreateCommentRequest } from 'types/article/remote';
 
-let REQUEST_URL = '';
-if (process.env.REACT_APP_MSW === 'development') {
-  REQUEST_URL = 'http://localhost:3000';
-} else {
-  REQUEST_URL = 'http://honeybees.community';
-}
+const REQUEST_URL = 'https://puzzled-cautious-radish.glitch.me';
 
 export const createArticle = async (body: CreateArticleRequest) => {
   await client.post(`${REQUEST_URL}/articles`, body);
